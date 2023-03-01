9438990c7e68737e3f9f32de2898d4f818cf4d22
andrey <andreyfaigon@gmail.com>
2023-02-28 22:01:30 -0500

    Add project files.

---
+++ AFPortfolio2022/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AFPortfolio2022.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult Projects()
+        {
+           
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            return View();
+        }
+        public ActionResult Experience()
+        {
+            return View();
+        }
+        public ActionResult Skills()
+        {
+            return View();
+        }
+    }
+}
---
+++ AFPortfolio2022/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AFPortfolio2022.MvcApplication" Language="C#" %>
